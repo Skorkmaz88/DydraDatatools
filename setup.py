59a04ff2a71d5399ad63ec24ce76955dacc98092
semih <skorkmaz88@yahoo.com>
2017-10-01 20:38:35 +0300

    Starters

---
+++ setup.py
@@ -0,0 +1,16 @@
+name='dydradata',
+version='0.1.0',
+description='Dydra data generation python package for testing Machine Learning integrations',
+author='Semih Korkmaz',
+author_email='contactsemih@gmail.com',
+license='Unlicensed',
+classifiers=[
+    'Development Status :: 3 - Alpha',s
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 2.7',
+],
+keywords='dydra machinelearningtools datagenerator data',
+install_requires=['numpy', ],
+python_requires='~=2.7',
